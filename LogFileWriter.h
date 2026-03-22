8b8baa6dc21cec06394a2029dd7682bd7189bc8f
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-03-22 07:29:58 +0100

    Update LogFileWriter.h

--- LogFileWriter.h
+++ LogFileWriter.h
@@ -7,12 +7,10 @@
 class LogFileWriter {
 
     private:
-
         // Contains the name of the file to manage
         std::string _filename;
 
     public:
-
         // Constructor
         LogFileWriter(const std::string &filename) : _filename(filename);
 
@@ -22,6 +20,6 @@ class LogFileWriter {
         void set_filename(std::string p_filename) { _filename = p_filename; }
         bool get_filename() const { return _filename; }
 
-    };
+};
 
 #endif
